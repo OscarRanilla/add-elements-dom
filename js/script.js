969b7b0100f4cfea819c5a4943d46205ea9cb7ea
OscarRanilla <oscarjosemanuel89@gmail.com>
2024-11-08 15:53:48 +0100

    add html,js,css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+ document.getElementById("agregar").addEventListener("click", function(){
+    const nuevoElemento = prompt("Introduce un elemento para la lista:");
+    if (nuevoElemento) {
+        const li = document.createElement("li");
+        li.textContent = nuevoElemento;
+        document.getElementById("lista").appendChild(li);
+    } else {
+    alert("No has introducido ningún elemento.");
+    }
+    });
+    
